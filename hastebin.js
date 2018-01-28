32effe21f0bb2aed3507ee4ff4cd5652452a6b90
Ruyi Li <ruyili2002@gmail.com>
2018-01-27 20:27:37 -0500

    i like quotes better

--- hastebin.js
+++ hastebin.js
@@ -1,6 +1,6 @@
 const snek = require("snekfetch");
 module.exports = async function(code, language = ""){
     let res = await snek.post("https://hastebin.com/documents").send(code).catch(console.error);
-    const url = `https://hastebin.com/${res.body.key}${language ? `.${language}` : ``}`;
+    const url = `https://hastebin.com/${res.body.key}${language ? `.${language}` : ""}`;
     return url;
 }
