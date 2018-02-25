01d83c457f17e49b0aee7bba97bae583e48f1e49
Ruyi Li <ruyili2002@gmail.com>
2018-02-25 13:57:44 -0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { promisified: p } = require('phin');
 module.exports = async function(code, lang = ''){
-    const res = await p({url: 'https://hastebin.com/documents', method: 'POST', data: code})
-    const { key } = JSON.parse(res.body);
-    return `https://hastebin.com/${key}${lang && `.${lang}`}`
+    try{
+        const res = await p({url: 'https://hastebin.com/documents', method: 'POST', data: code})
+        const { key } = JSON.parse(res.body);
+        return `https://hastebin.com/${key}${lang && `.${lang}`}`
+    }catch(err){
+        throw err;
+    }
 }
--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-require('./index')(require('fs').readFileSync(__filename), 'js').then(r => console.log(r))
+require('./index')(require('fs').readFileSync(__filename, 'utf-8'), 'js').then(r => console.log(r)).catch(console.error)
 //https://hastebin.com/qoziyevubu.js
