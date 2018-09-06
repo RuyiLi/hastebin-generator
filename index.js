1a5b6d13ebb2b4281f35e1c60405e75087041641
Abaaaaaaady <gamersspeaks@gmail.com>
2018-09-06 21:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { promisified: p } = require('phin');
 module.exports = async function(code, lang = ''){
     try{
-        const res = await p({url: 'https://hastebin.com/documents', method: 'POST', data: code})
-        const { key } = JSON.parse(res.body);
+        const res = await p({url: 'https://hastebin.com/documents', method: 'POST', data: code, parse: 'json'})
+        const { key } = res.body
         return `https://hastebin.com/${key}${lang && `.${lang}`}`
     }catch(err){
         throw err;
